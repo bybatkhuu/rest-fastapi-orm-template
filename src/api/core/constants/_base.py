8d1a5914ccbf4ece3fe90658fa52967505182830
Batkhuu Byambajav <batkhuu10@gmail.com>
2026-03-04 10:51:38 +0900

    fix: update API_SLUG to reflect the correct project name

--- src/api/core/constants/_base.py
+++ src/api/core/constants/_base.py
@@ -2,7 +2,7 @@ ENV_PREFIX = "FOT_"
 ENV_PREFIX_API = f"{ENV_PREFIX}API_"
 ENV_PREFIX_DB = f"{ENV_PREFIX}DB_"
 
-API_SLUG = "rest-fastapi-template"
+API_SLUG = "rest-fastapi-orm-template"
 
 
 __all__ = [
