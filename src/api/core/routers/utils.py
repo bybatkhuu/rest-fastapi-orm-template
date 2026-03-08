b911b18308b984d97ebbb83b47c2ed5ac5a249a8
Batkhuu Byambajav <batkhuu10@gmail.com>
2026-03-08 10:04:22 +0900

    feat: enhance health check endpoint to include database connectivity status

--- src/api/core/routers/utils.py
+++ src/api/core/routers/utils.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Request
 
 from api.core.schemas import BaseResPM
 from api.core.responses import BaseResponse
+from api.databases.rdb import async_is_db_connectable, async_read_engine
 
 router = APIRouter(tags=["Utils"])
 
@@ -36,12 +37,24 @@ async def get_ping(request: Request):
 )
 async def get_health(request: Request):
     _message = "Everything is OK."
-    _data = {"api": {"message": "API is up.", "is_alive": True}}
+    _data = {
+        "api": {"message": "API is up.", "is_alive": True},
+        "db": {"message": "Database status is unknown.", "is_alive": None},
+    }
+    _status_code = 200
+
+    if await async_is_db_connectable(async_engine=async_read_engine):
+        _data["db"] = {"message": "Database is connected.", "is_alive": True}
+    else:
+        _status_code = 503
+        _message = "One or more services are unavailable!"
+        _data["db"] = {"message": "Database connection failed!", "is_alive": False}
 
     return BaseResponse(
         request=request,
-        content=_data,
+        status_code=_status_code,
         message=_message,
+        content=_data,
         headers={
             "Cache-Control": "no-cache, no-store, must-revalidate",
             "Pragma": "no-cache",
